226ce0451d991e6ea4c109151c53718656304d18
X23009 <x23009xx@aitech.ac.jp>
2024-07-14 00:09:03 +0900

    7/13-01

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loading = document.getElementById("loading");
+    window.onload = function() {
+        setTimeout(() =>{
+            loading.classList.add("loaded");
+        }, 1300);
+    }
+});
+
