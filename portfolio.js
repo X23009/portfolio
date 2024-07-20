1530e26a802de7e34f9983761dc6149a243fc68c
X23009 <x23009xx@aitech.ac.jp>
2024-07-21 00:54:56 +0900

    7/20-02

--- portfolio.js
+++ portfolio.js
@@ -9,3 +9,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    const elementsToObserve = document.querySelectorAll('.fade-in');
+    elementsToObserve.forEach(element => {
+        observer.observe(element);
+    });
+});
